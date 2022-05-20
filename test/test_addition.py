a100c2aaed9c3755caeabe659443ff0d75b04a11
huytrong <tronghuypbc@gmail.com>
2022-05-20 21:12:22 +0700

    test 4

--- test/test_addition.py
+++ test/test_addition.py
@@ -1,9 +1,13 @@
-from src.calculator import add
+from src.calculator import add, subtract
 import pytest
 
 def test_add():
     result = add(2, 3)
-    assert result == 2, 'Add succeed'
+    assert result == 5, 'Add succeed'
+
+def test_subtract():
+    result = subtract(3, 1)
+    assert result == 2
 
 def test_add_string():
     with pytest.raises(TypeError):
