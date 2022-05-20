ff604db843c063d08520c7237f85bf297003b601
huytrong <tronghuypbc@gmail.com>
2022-05-20 21:02:46 +0700

    test

---
+++ test/test_addition.py
@@ -0,0 +1,10 @@
+from src.calculator import add
+import pytest
+
+def test_add():
+    result = add(2, 3)
+    assert result == 5, 'Add succeed'
+
+def test_add_string():
+    with pytest.raises(TypeError):
+        add('ditme', 5)
